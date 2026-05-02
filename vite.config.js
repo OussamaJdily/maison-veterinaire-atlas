9a2ce08b5f38fbd534dd0840ab59d5df2af2f558
OussamaJdily <125580487+OussamaJdily@users.noreply.github.com>
2026-05-02 20:58:41 +0100

    Change base path in Vite configurafix: use relative base path for better deployment compatibilitytion

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/maison-veterinaire-atlas/",
+    base: "./",
   plugins: [react()],
   build: {
     rollupOptions: {
